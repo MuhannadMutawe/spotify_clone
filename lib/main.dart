e1f51a4152da8c910aa5d5e7605405b4547830e4
Muhannad R Mutawe <muhannadrm208@gmail.com>
2026-02-18 10:27:56 +0200

    add bloc provider to material app & change mode method to choose mode button widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:spotify_app/core/config/themes/app_theme.dart';
 import 'package:spotify_app/core/utils/app_router.dart';
+import 'package:spotify_app/presentation/choose_mode/manger/theme_cubit.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,15 +23,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(390, 844),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (context, child) => MaterialApp.router(
-        routerConfig: AppRouter.router,
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme.lightTheme,
-        darkTheme: AppTheme.darkTheme,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => ThemeCubit(),
+        ),
+      ],
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder: (context, themeMode) {
+          return ScreenUtilInit(
+            designSize: const Size(390, 844),
+            minTextAdapt: true,
+            splitScreenMode: true,
+            builder: (context, child) => MaterialApp.router(
+              routerConfig: AppRouter.router,
+              debugShowCheckedModeBanner: false,
+              themeMode: themeMode,
+              theme: AppTheme.lightTheme,
+              darkTheme: AppTheme.darkTheme,
+            ),
+          );
+        },
       ),
     );
   }
--- lib/presentation/choose_mode/view/widgets/choose_mode_buttons.dart
+++ lib/presentation/choose_mode/view/widgets/choose_mode_buttons.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spotify_app/presentation/choose_mode/manger/theme_cubit.dart';
 import 'package:spotify_app/presentation/choose_mode/view/widgets/mode_button.dart';
 
 import '../../../../core/config/assets/app_vectors.dart';
@@ -16,10 +18,12 @@ class ChooseModeButtons extends StatelessWidget {
         ModeButton(
           label: 'Dark Mode',
           iconLink: AppVectors.moon,
+          onTap: () => context.read<ThemeCubit>().changeMode(ThemeMode.dark),
         ),
         ModeButton(
           label: 'Light Mode',
           iconLink: AppVectors.sun,
+          onTap: () => context.read<ThemeCubit>().changeMode(ThemeMode.light),
         )
       ],
     );
