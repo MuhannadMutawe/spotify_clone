a9a0d2e87f7f0e5ea64861d147f9cfbfbe00916d
Muhannad R Mutawe <muhannadrm208@gmail.com>
2026-03-02 22:34:39 +0200

    create create user req model & auth firebase source implementation.

---
+++ lib/data/models/auth/create_user_req.dart
@@ -0,0 +1,9 @@
+class CreateUserReq {
+  final String fullName, email, password;
+
+  CreateUserReq({
+    required this.fullName,
+    required this.email,
+    required this.password,
+  });
+}
--- lib/data/sources/auth/auth_firebase_source.dart
+++ lib/data/sources/auth/auth_firebase_source.dart
@@ -1,19 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:spotify_app/data/models/auth/create_user_req.dart';
+
 abstract class AuthFirebaseSource {
-  Future<void> signup();
+  Future<void> signup(CreateUserReq user);
 
   Future<void> signin();
 }
 
-class AuthFirebaseSourceImplementation extends AuthFirebaseSource{
+class AuthFirebaseSourceImplementation extends AuthFirebaseSource {
+  @override
+  Future<void> signin() async {}
 
   @override
-  Future<void> signin() async {
-
+  Future<void> signup(CreateUserReq user) async {
+    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: user.email,
+      password: user.password,
+    );
   }
-
-  @override
-  Future<void> signup() async {
-
-  }
-
 }
