543f84c0db0534884594e8440533726445d41019
Muhannad R Mutawe <muhannadrm208@gmail.com>
2026-03-10 16:37:56 +0200

    refactor sign in view body.

--- lib/presentation/auth/views/widgets/signin_view_body.dart
+++ lib/presentation/auth/views/widgets/signin_view_body.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:go_router/go_router.dart';
-import 'package:spotify_app/common/helper/is_dark_mode.dart';
-import 'package:spotify_app/core/config/themes/app_colors.dart';
-import 'package:spotify_app/core/utils/app_router.dart';
-import 'package:spotify_app/presentation/auth/manger/Sign_in/sign_in_cubit.dart';
-import 'package:spotify_app/presentation/auth/manger/Sign_in/sign_in_state.dart';
 import 'package:spotify_app/presentation/auth/views/widgets/custom_page_title.dart';
+import 'package:spotify_app/presentation/auth/views/widgets/sign_in_bloc_listener.dart';
 import 'package:spotify_app/presentation/auth/views/widgets/signin_text_form_fields_and_signin_button.dart';
 
 class SigninViewBody extends StatelessWidget {
@@ -32,77 +26,4 @@ class SigninViewBody extends StatelessWidget {
       ),
     );
   }
-}
-
-class SignInBlocListener extends StatelessWidget {
-  const SignInBlocListener({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocListener<SignInCubit, SignInState>(
-      listenWhen: (previous, current) =>
-          current is SignInLoading ||
-          current is SignInSuccess ||
-          current is SignInFailure,
-      listener: (context, state) => state.whenOrNull(
-        loading: () {
-          showDialog(
-            context: context,
-            builder: (context) => const Center(
-              child: CircularProgressIndicator(
-                color: AppColors.primary,
-              ),
-            ),
-          );
-          return null;
-        },
-        success: () {
-          context.pushReplacement(AppRouter.kHomeView);
-          return null;
-        },
-        failure: (errorMessage) {
-          setupErrorState(context, errorMessage);
-          return null;
-        },
-      ),
-      child: SizedBox.shrink(),
-    );
-  }
-}
-
-void setupErrorState(BuildContext context, String error) {
-  context.pop();
-  showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      icon: const Icon(
-        Icons.error,
-        color: Colors.red,
-        size: 32,
-      ),
-      content: Text(
-        error,
-        style: TextStyle(
-          fontSize: 16,
-          fontWeight: FontWeight.w500,
-          color: context.isDarkMode ? Colors.white : Colors.black,
-        ),
-      ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            context.pop();
-          },
-          child: Text(
-            'Got it',
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 14,
-              color: AppColors.primary,
-            ),
-          ),
-        ),
-      ],
-    ),
-  );
 }
