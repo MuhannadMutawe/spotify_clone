279d7a32019070d34a2578fa61a1dd3eea56971a
MuhannadMutawe <muhannadrm208@gmail.com>
2026-03-24 16:34:24 +0200

    create user collection in signup method,

--- lib/data/sources/auth/auth_firebase_source.dart
+++ lib/data/sources/auth/auth_firebase_source.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:spotify_app/data/models/auth/create_user_req.dart';
 import 'package:spotify_app/data/models/auth/signin_user_req.dart';
@@ -12,16 +13,20 @@ class AuthFirebaseSourceImplementation extends AuthFirebaseSource {
   @override
   Future<void> signin(SignInUserReq userInfo) async {
     await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: userInfo.email,
-        password:userInfo.password
+      email: userInfo.email,
+      password: userInfo.password,
     );
   }
 
   @override
   Future<void> signup(CreateUserReq user) async {
-    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+    var data = await FirebaseAuth.instance.createUserWithEmailAndPassword(
       email: user.email,
       password: user.password,
     );
+    await FirebaseFirestore.instance.collection('Users').add({
+      'name': user.fullName,
+      'email': data.user?.email,
+    });
   }
 }
