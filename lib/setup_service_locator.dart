335c4d4c38066c33d9535afb9d2c2f28202d1b73
MuhannadMutawe <muhannadrm208@gmail.com>
2026-03-28 18:56:17 +0300

    create get news songs use case file and set up  Song Repository Implementation on server locator.

--- lib/setup_service_locator.dart
+++ lib/setup_service_locator.dart
@@ -1,8 +1,11 @@
 import 'package:get_it/get_it.dart';
 import 'package:spotify_app/data/repository/auth/auth_repository_implementation.dart';
+import 'package:spotify_app/data/repository/song/song_repository_implementation.dart';
 import 'package:spotify_app/data/sources/auth/auth_firebase_source.dart';
+import 'package:spotify_app/data/sources/song/song_firebase_source.dart';
 import 'package:spotify_app/domain/usecases/auth/signin_use_case.dart';
 import 'package:spotify_app/domain/usecases/auth/signup_use_case.dart';
+import 'package:spotify_app/domain/usecases/song/get_news_songs_use_case.dart';
 
 final getIt = GetIt.instance;
 
@@ -20,6 +23,18 @@ void setupServiceLocator() {
       AuthRepositoryImplementation(
         AuthFirebaseSourceImplementation(),
       ),
+    ),
+  );
+
+  getIt.registerSingleton<SongRepositoryImplementation>(
+    SongRepositoryImplementation(
+      SongFirebaseSourceImplementation(),
+    ),
+  );
+
+  getIt.registerSingleton<GetNewsSongsUseCase>(
+    GetNewsSongsUseCase(
+      getIt<SongRepositoryImplementation>(),
     ),
   );
 }
