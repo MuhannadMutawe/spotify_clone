99c600886a075f0b0ab7a6ecf1e558ef335c2f38
MuhannadMutawe <muhannadrm208@gmail.com>
2026-03-30 20:45:34 +0300

    add slider widget to song player view body.

--- lib/presentation/song_player/view/widgets/song_player_view_body.dart
+++ lib/presentation/song_player/view/widgets/song_player_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:spotify_app/core/config/themes/app_colors.dart';
 import 'package:spotify_app/domain/entities/song/song_entity.dart';
 import 'package:spotify_app/presentation/song_player/view/widgets/song_detail.dart';
 import 'package:spotify_app/presentation/song_player/view/widgets/song_image_cover.dart';
@@ -26,6 +27,51 @@ class SongPlayerViewBody extends StatelessWidget {
           ),
           SongDetail(
             songEntity: songEntity,
+          ),
+          Column(
+            children: [
+              Slider(
+                value: 3,
+                min: 0.0,
+                max: 5.5,
+                onChanged: (value) {},
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //   children: [
+              //     Text(
+              //       // formatDuration(
+              //       //   context.read<SongPlayerCubit>().songPosition,
+              //       // ),
+              //     ),
+
+              //     Text(
+              //       // formatDuration(
+              //       //   context.read<SongPlayerCubit>().songDuration,
+              //       // ),
+              //     ),
+              //   ],
+              // ),
+              const SizedBox(
+                height: 20,
+              ),
+
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  height: 60,
+                  width: 60,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: AppColors.primary,
+                  ),
+                  child: Icon(Icons.play_arrow),
+                ),
+              ),
+            ],
           ),
         ],
       ),
