51491e92e8471b1e49e2264b934b61bbdb767286
MuhannadMutawe <muhannadrm208@gmail.com>
2026-04-06 18:12:00 +0300

    add app bar to profile view.

--- lib/presentation/profile/view/profile_view.dart
+++ lib/presentation/profile/view/profile_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:spotify_app/common/widgets/app_bar.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: BasicAppBar(),
+    );
   }
 }
